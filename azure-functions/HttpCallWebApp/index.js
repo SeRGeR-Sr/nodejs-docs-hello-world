8a50833b94b0206d1b82c1609718c102ab3b4f1a
Sargon GERGES <gergessargon@gmail.com>
2025-12-03 22:21:13 +0200

    function debugging 2

--- azure-functions/HttpCallWebApp/index.js
+++ azure-functions/HttpCallWebApp/index.js
@@ -1,25 +1,56 @@
-const WEB_APP_URL = "https://Sargon_WebApp.azurewebsites.net";
+const https = require("https");
+const url = require("url");
+
+const WEB_APP_URL = "https://Sargon-WebApp.azurewebsites.net/";
+
+function getUrl(targetUrl) {
+  return new Promise((resolve, reject) => {
+    const options = url.parse(targetUrl);
+    options.method = "GET";
+
+    const req = https.request(options, res => {
+      let data = "";
+
+      res.on("data", chunk => {
+        data += chunk;
+      });
+
+      res.on("end", () => {
+        resolve({ statusCode: res.statusCode, body: data });
+      });
+    });
+
+    req.on("error", err => {
+      reject(err);
+    });
+
+    req.end();
+  });
+}
 
 module.exports = async function (context, req) {
   context.log("HttpCallWebApp function triggered");
 
   try {
-    // Built-in fetch is available in Node 18/20/22 on Azure Functions v4+
-    const response = await fetch(WEB_APP_URL);
-    const text = await response.text();
+    const result = await getUrl(WEB_APP_URL);
+
+    context.log(
+      `HttpCallWebApp: got status ${result.statusCode} from ${WEB_APP_URL}`
+    );
 
     context.res = {
       status: 200,
       headers: { "Content-Type": "text/plain" },
-      body: `Function called the Web App (${WEB_APP_URL}) and got:\n\n${text}`
+      body:
+        `Web App responded with status ${result.statusCode}.\n\n` +
+        `Body:\n${result.body}`
     };
-
   } catch (err) {
     context.log("Error calling web app:", err);
 
     context.res = {
       status: 500,
-      body: "Error calling web app. Check Function logs."
+      body: "Error calling web app. Check HttpCallWebApp logs for details."
     };
   }
 };
